6caba73960e817d4028bea370f9076904a51856d
vladimir-tutin <patrick.d.coy@gmail.com>
2019-08-14 17:56:18 -0400

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -7,20 +7,27 @@ import sys
 url = "http://192.168.1.#:32400"
 token = "##################"
 
+if hasattr(__builtins__, 'raw_input'):
+        input=raw_input
+
 plex = PlexServer(url, token)
 for i, playlist in enumerate(plex.playlists()):
-	print i+1, ")", playlist.title
+        print("{position}) {playlist_title}".format(position=i+1, playlist_title=playlist.title))
 choice = -1
 while choice == -1:
-	selection = raw_input("Select playlist: ")
-	try:
-		selection = int(selection)
-		if selection > 0 and selection <= i+1:
-			choice = selection - 1
-		else:
-			print "Invalid selection"
-	except:
-		print "Invalid selection"
-new_playlist_name = raw_input("Enter new playlist name: ")
-Playlist.create(plex, new_playlist_name, plex.playlists()[choice].items())
+        selection = input("Select playlist: ")
+        try:
+                selection = int(selection)
+                if selection > 0 and selection <= i+1:
+                        choice = selection - 1
+                else:
+                        print("Invalid selection")
+        except:
+                print("Invalid selection")
+new_playlist_name = input("Enter new playlist name: ")
+try:
+    Playlist.create(plex, new_playlist_name, plex.playlists()[choice].items())
+    print("{playlist} created".format(playlist=new_playlist_name))
+except:
+    print("Error")
 
